85990f0a08ff1e76c9a43f746ed8f9c171528a75
Ernesto Antonio Rodriguez Acosta <ernestoacostacuba@yahoo.com.mx>
2022-07-23 14:07:15 +0200

    Integration tests for the WebFlux slice (OrderControllerWebFluxTests.class)

--- src/test/java/com/polarbookshop/orderservice/application/api/rest/OrderControllerWebFluxTests.java
+++ src/test/java/com/polarbookshop/orderservice/application/api/rest/OrderControllerWebFluxTests.java
@@ -1,48 +1,28 @@
 package com.polarbookshop.orderservice.application.api.rest;
 
-import com.polarbookshop.orderservice.application.api.client.BookClient;
 import com.polarbookshop.orderservice.application.service.OrderService;
-import com.polarbookshop.orderservice.application.service.OrderServiceImpl;
 import com.polarbookshop.orderservice.domain.dto.OrderRequest;
-import com.polarbookshop.orderservice.domain.repository.OrderRepository;
 import com.polarbookshop.orderservice.infrastructure.entity.OrderEntity;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Mono;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.BDDMockito.given;
 
 @WebFluxTest(OrderController.class)
+@ActiveProfiles("integration")
 class OrderControllerWebFluxTests {
 
     @Autowired
     private WebTestClient webClient;
 
-//    @Mock
-//    private BookClient bookClient;
-//
-//    @Mock
-//    private OrderRepository orderRepository;
-
     @MockBean
     private OrderService orderService;
-
-//    @BeforeEach
-//    void setUp() {
-//        MockitoAnnotations.initMocks(this);
-//        orderService = new OrderServiceImpl(bookClient, orderRepository);
-//    }
-
-//    @BeforeEach
-//    void setUp() {
-//        orderService = Mockito.mock(OrderService.class);
-//    }
 
     @Test
     void whenBookNotAvailableThenRejectOrder() {
